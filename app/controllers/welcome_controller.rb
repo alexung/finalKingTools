a67020eeab41577a851926c18a71de89f2a711d9
Alex Ung <alexung.ucsb@gmail.com>
2015-12-20 23:24:25 -0800

    all categories in the category bar

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,6 @@ class WelcomeController < ApplicationController
   def index
       @q = Product.ransack(params[:q])
       @product = @q.result.paginate(:page => params[:page], :per_page => 9)
+      @products = Product.uniq.pluck(:category)
   end
 end
